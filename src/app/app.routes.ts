9755078359913459774429d5f0dc4fe3102ef5c5
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-08 16:06:40 -0300

    feat: add wildcard route to redirect unmatched paths to auth/login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -32,5 +32,6 @@ export const routes: Routes = [
       { path: 'details/:id', component: MovieDetails },
       { path: 'create', component: CreateMovie },
     ]
-  }
+  },
+  { path: "**", redirectTo: 'auth/login', pathMatch: 'full', }
 ];
