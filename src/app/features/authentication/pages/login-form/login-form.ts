a9d620f41707453829a16ebc45594f2292b923af
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-08 17:13:03 -0300

    feat: implement reactive form for login with validation messages

--- src/app/features/authentication/pages/login-form/login-form.ts
+++ src/app/features/authentication/pages/login-form/login-form.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { email, form, Field, minLength, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [Field],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css',
 })
 export class LoginForm {
+  loginModel = signal({
+    email: '',
+    password: '',
+  });
+
+  loginForm = form(this.loginModel, (schemaPath) => {
+    required(schemaPath.email, {message: 'O e-mail é obrigatório.'});
+    email(schemaPath.email, {message: 'E-mail inválido.'});
+    required(schemaPath.password, {message: 'A senha é obrigatória.'});
+    minLength(schemaPath.password, 8, { message: 'A senha deve ter no mínimo 8 caracteres.'})
+  });
 
 }
