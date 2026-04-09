082dd27d51200d21fed6c8ad56c8837b8ee2a72a
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-09 12:10:02 -0300

    feat: implement user registration form with validation and confirm password functionality

--- src/app/features/authentication/pages/register-user-form/register-user-form.ts
+++ src/app/features/authentication/pages/register-user-form/register-user-form.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { email, Field, form, minLength, required, validate } from '@angular/forms/signals';
+import { confirmPassword } from '../../validators/confirm-password';
 
 @Component({
   selector: 'app-register-user-form',
-  imports: [],
+  imports: [Field],
   templateUrl: './register-user-form.html',
   styleUrl: './register-user-form.css',
 })
 export class RegisterUserForm {
+  registerModel = signal({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
 
+  registerForm = form(this.registerModel, (schemaPath) => {
+    required(schemaPath.name, { message: 'O nome é obrigatório.'});
+    
+    required(schemaPath.email, { message: 'O e-mail é obrigatório.'});
+    email(schemaPath.email, { message: 'E-mail inválido.'});
+
+    required(schemaPath.password, { message: 'A senha é obrigatória.'});
+    minLength(schemaPath.password, 8 , { message: 'A senha deve ter no mínimo 8 caracteres.'});
+
+    confirmPassword(schemaPath.confirmPassword, schemaPath.password);
+  });
 }
