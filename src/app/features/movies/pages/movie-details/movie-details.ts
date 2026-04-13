5cba0fb89ae1e47c4607b6ce2fb43d5bf3f8b16d
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-13 12:33:41 -0300

    feat: enhance movie details page with dynamic data binding and error handling

--- src/app/features/movies/pages/movie-details/movie-details.ts
+++ src/app/features/movies/pages/movie-details/movie-details.ts
@@ -1,25 +1,35 @@
-import { Component, inject, input, signal, WritableSignal } from '@angular/core';
+import { Component, inject, input, linkedSignal, signal, WritableSignal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { MoviesApi } from '../../services/movies-api';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-details',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './movie-details.html',
   styleUrl: './movie-details.css',
 })
 export class MovieDetails {
   private readonly _moviesApi = inject(MoviesApi);
   
+  readonly BASE_URL = "http://localhost:3000";
+
   id = input.required<string>();
 
   movieDetailsResource = rxResource({
-    params: () => this.id,
-    stream: ({params}) => this._moviesApi.getMovieDetails(+params)
-  })
+    params: () => this.id(),
+    stream: ({params}) => this._moviesApi.getMovieDetails(+params),
+  });
 
+  movieDetails = linkedSignal(()=> {
+    const ERROR_ON_RESPONSE = !!this.movieDetailsResource.error();
+    
+    if(ERROR_ON_RESPONSE) return undefined;
+
+    return this.movieDetailsResource.value();
+  })
   //////////////////////////////////////////////////
-  
+
   reviewsCount = 5;
   // Apenas uma array de 5 elementos para o @for loop
   stars = new Array(5);
