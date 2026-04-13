efd62ba9acf547c77000230ff82e835e6e2c5fdd
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-13 16:26:52 -0300

    feat: add movie rating functionality and update movie details with new ratings

--- src/app/features/movies/pages/movie-details/movie-details.ts
+++ src/app/features/movies/pages/movie-details/movie-details.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, input, linkedSignal, signal, WritableSigna
 import { rxResource } from '@angular/core/rxjs-interop';
 import { MoviesApi } from '../../services/movies-api';
 import { DecimalPipe } from '@angular/common';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-movie-details',
@@ -28,7 +29,6 @@ export class MovieDetails {
 
     return this.movieDetailsResource.value();
   })
-  //////////////////////////////////////////////////
 
   isFavorite = signal(false);
   currentRating= signal<number| undefined>(undefined);
@@ -37,6 +37,17 @@ export class MovieDetails {
     const rating = this.currentRating() ?? 0;
     const boolArray = [0, 1, 2, 3, 4].map((index) => index < rating);  
     return boolArray;
+  });
+
+  rateMovieResource = rxResource({
+    params: () => {
+      const rating = this.currentRating() ?? 0;
+      if(rating > 0) return { id: +this.id(), rating }
+      return undefined;
+    },
+    stream: ({params}) => this._moviesApi.rateMovie(params.id, params.rating).pipe(
+      tap((movieUpdated)=> this.movieDetails.set(movieUpdated))
+    ),
   });
 
   toggleFavorite() {
