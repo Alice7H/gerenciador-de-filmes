b7616e17dbebcd2c90a9518d99b99e7c18b54a8d
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-14 15:57:30 -0300

    feat: implement form binding with signals and image upload preview in createMovies

--- src/app/features/movies/pages/create-movie/create-movie.ts
+++ src/app/features/movies/pages/create-movie/create-movie.ts
@@ -1,54 +1,50 @@
 import { Component, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-movie',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-movie.html',
   styleUrl: './create-movie.css',
 })
 export class CreateMovie {
-  // Sinal para armazenar o URL da pré-visualização da imagem
-  imagePreview: WritableSignal<string | null> = signal(null);
+  title = signal<string>('');
+  year = signal<number | undefined>(undefined);
+  category = signal<string>('');
+  description = signal<string>('');
+ 
+  imagePreview = signal<string | undefined>(undefined);
+  selectedFile = signal<File | undefined>(undefined);
 
-  /**
-   * Manipula a seleção de arquivo pelo input.
-   * Abre o explorador de arquivos novamente se a imagem for clicada.
-   */
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
-
-    // Certifica-se de que um arquivo foi selecionado
-    if (input.files && input.files[0]) {
+    if(input.files && input.files[0]){
       const file = input.files[0];
+      this.selectedFile.set(file);
 
-      // Verifica se o arquivo é uma imagem
-      if (file.type.startsWith('image/')) {
-        const reader = new FileReader();
-
-        // Quando o arquivo é lido, armazena o resultado (base64) no sinal
-        reader.onload = (e) => {
-          this.imagePreview.set(e.target?.result as string);
-        };
-
-        // Lê o arquivo como URL de dados (Base64)
-        reader.readAsDataURL(file);
-      } else {
-        alert('Por favor, selecione um arquivo de imagem.');
-        // Opcional: Limpa o input se o arquivo não for uma imagem
-        input.value = '';
-        this.imagePreview.set(null);
+      if(this.imagePreview()){
+        URL.revokeObjectURL(this.imagePreview()!);
       }
+
+      const objectUrl = URL.createObjectURL(file);
+
+      this.imagePreview.set(objectUrl);
     }
   }
 
-  // Opcional: Adicione métodos para Salvar e Cancelar
   salvar() {
+    console.log('title', this.title());
+    console.log('year', this.year());
+    console.log('category', this.category());
+    console.log('description', this.description());
+    
+    console.log('imagePreview', this.imagePreview());
+    console.log('selectedFile', this.selectedFile());
+
     console.log('Filme salvo!');
-    // Implemente a lógica de envio do formulário aqui
   }
 
   cancelar() {
     console.log('Operação cancelada!');
-    // Implemente a lógica de navegação de volta ou fechamento de modal aqui
   }
 }
