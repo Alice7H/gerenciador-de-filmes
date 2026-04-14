3277476599d2a301ec124ba363d0493ddd716420
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-14 17:13:43 -0300

    feat: update navigation in create movie and explore movies pages

--- src/app/features/movies/pages/create-movie/create-movie.ts
+++ src/app/features/movies/pages/create-movie/create-movie.ts
@@ -3,10 +3,11 @@ import { rxResource } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { MoviesApi } from '../../services/movies-api';
 import { setErrorMessage } from '../../../../shared/utils/set-error-message';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-create-movie',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './create-movie.html',
   styleUrl: './create-movie.css',
 })
@@ -63,7 +64,4 @@ export class CreateMovie {
     this.movieFormData.set(formData);
   }
 
-  cancelar() {
-    console.log('Operação cancelada!');
-  }
 }
--- src/app/features/movies/pages/explore-movies/explore-movies.ts
+++ src/app/features/movies/pages/explore-movies/explore-movies.ts
@@ -43,8 +43,6 @@ export class ExploreMovies {
     });
   })
 
-  adicionarFilme() {}
-
   clearFilter() {
     this.movieTitleFilter.set('');
     this.movieCategoryFilter.set('');
