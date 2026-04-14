c7df5f8f46f12400bbd742d61942fda0434492e6
Alice H. M. Sousa <alicehata9@gmail.com>
2026-04-14 15:54:07 -0300

    feat: implement createMovie API and redirect to create page

--- src/app/features/movies/services/movies-api.ts
+++ src/app/features/movies/services/movies-api.ts
@@ -22,4 +22,8 @@ export class MoviesApi {
       rating,
     });
   }
+
+  createMovie(movieData: FormData){
+    return this._httpClient.post<IMovieResponse>('http://localhost:3000/movies', movieData);
+  }
 }
